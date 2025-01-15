5ecc7ecfe5d05dfb214c0486b6512cec7a6ee2d4
Emi Vatavu <emilianvatavu@gmail.com>
2025-01-15 03:36:34 +0200

    Adaugat functie pentru verificarea Ip-urilor cu dns

--- verif_hosts.sh
+++ verif_hosts.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-while read -r ip name; do
-x=$(nslookup "$name" | awk '/Address: / {print $2}' | tail -n 1)
+check_ip()
+{
+local name=$1
+local ip=$2
+local server=$3
+x=$(nslookup "$name" "$server" | awk '/Address: / {print $2}' | tail -n 1)
 if [[ "$x" != "$ip" ]]; then
-echo "Bogus IP for $name in /etc/hosts!"
+echo "Bogus IP for $name"
 else
 echo "$name IP is correct."
 fi
+
+}
+while read -r ip name; do
+	check_ip "$name" "$ip" "8.8.8.8"
 done < /etc/hosts
